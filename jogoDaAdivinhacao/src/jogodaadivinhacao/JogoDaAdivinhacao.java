5b185c5c622c5feee3b4972983b30d31912ff1b6
biehlarthur <biehlarthurv@gmail.com>
2024-05-27 17:17:38 -0300

    entra e saida perguntas

--- jogoDaAdivinhacao/src/jogodaadivinhacao/JogoDaAdivinhacao.java
+++ jogoDaAdivinhacao/src/jogodaadivinhacao/JogoDaAdivinhacao.java
@@ -14,9 +14,10 @@ public class JogoDaAdivinhacao {
 
             int numero;
             int escolher;
-            int chancesF = 32;
-            int chancesM = 17;
-            int chancesD = 7;
+            int chancesF = 31;
+            int chancesM = 16;
+            int chancesD = 6;
+            int chancesI = 11;
             int aleatorio = random.nextInt(100);
             String nome;
 
@@ -27,9 +28,9 @@ public class JogoDaAdivinhacao {
             System.out.println("Bem vindo ao jogo de ADIVINHACAO");
             System.out.println("Voce quer comecar a jogar? 1.SIM | 2.NAO");
             escolher = ler.nextInt();
-            if(escolher == 1){
+            if (escolher == 1) {
                 System.out.println("VAMOOO!!");
-            }else{
+            } else {
                 System.out.println("ADIOS");
                 break;
             }
@@ -39,6 +40,7 @@ public class JogoDaAdivinhacao {
             System.out.println("1. FACIL: numeros de 1 a 100, 30 tentativas");
             System.out.println("2. MEDIO: numeros 1 a 100, 15 tentativas");
             System.out.println("3. DIFICIL: numeros 1 a 100, 5 tentativas");
+            System.out.println("4. IMPOSSIVEL: numeros 1 a 1000, 10 tentativas");
             System.out.println("====================");
             System.out.print("Escolha o MODO: ");
             escolher = ler.nextInt();
@@ -66,7 +68,7 @@ public class JogoDaAdivinhacao {
 
                         }
 
-                        System.out.println("Voce esta na tentatica: " + i);
+                        System.out.println("Voce esta na tentativa: " + i);
 
                     }
                     break;
@@ -91,7 +93,7 @@ public class JogoDaAdivinhacao {
 
                         }
 
-                        System.out.println("Voce esta na tentatica: " + i);
+                        System.out.println("Voce esta na tentativa: " + i);
 
                     }
 
@@ -118,10 +120,46 @@ public class JogoDaAdivinhacao {
 
                         }
 
-                        System.out.println("Voce esta na tentatica: " + i);
+                        System.out.println("Voce esta na tentativa: " + i);
                     }
                     break;
+                case 4:
+                    for (int i = 0; i < chancesI; i++) {
 
+                        System.out.println("Qual seu palpite?");
+                        numero = ler.nextInt();
+
+                        if (numero == aleatorio) {
+                            System.out.println("VOCE REALMENTE ACERTOR, MEUS PARABENS!!");
+                            System.out.println("====================");
+
+                            break;
+                        } else if (numero < aleatorio) {
+                            System.out.println("Tente um numero maior");
+                            System.out.println("====================");
+
+                        } else if (numero > aleatorio) {
+                            System.out.println("Tente um numero menor");
+                            System.out.println("====================");
+
+                        }
+
+                        System.out.println("Voce esta na tentativa: " + i);
+
+                    }
+
+                    break;
+
+            }
+
+            System.out.println("Deseja continuar? 1.SIM | 2.NAO");
+            escolher = ler.nextInt();
+
+            if (escolher == 1) {
+
+            } else {
+                System.out.println("BELEZA MEU MANO, VAI COM DEUS TMJ");
+                break;
             }
 
         } while (true);
